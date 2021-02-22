e1c7fc30e3fe6f3f0ace872332c826b1e698f918
jcarpiog <77985466+jcarpiog@users.noreply.github.com>
2021-02-22 04:21:15 +0100

    Add files via upload

---
+++ Paginas.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Switch, Route} from "react-router-dom"
+import {Inicio} from "./Inicio"
+import { ProductosLista } from "./Productos/index"
+import {ProductoDetalles} from "./Productos/ProductoDetalles"
+
+export const Paginas = () => {
+    return (
+        <section>
+            <Switch>
+                <Route path="/" exact component={Inicio}/>
+                <Route path="/productos" exact component={ProductosLista}/>
+                <Route path="/producto/:id" exact component={ProductoDetalles}/>
+            </Switch>
+        </section>
+    )
+}
---
+++ ProductoItem.js
@@ -0,0 +1,39 @@
+import React, {useContext} from 'react'
+import {Link} from "react-router-dom"
+import {DataContext} from "../../context/Dataprovider"
+
+export const ProductoItem = ({
+  id,
+  title,
+  price,
+  image,
+  category,
+}) => {
+
+  const value = useContext(DataContext);
+  const addCarrito = value.addCarrito;
+
+  console.log(image)
+    return (
+        <div className="producto">
+        <Link to={`/producto/${id}`}>
+          <div className="producto_img">
+            <img src={image.default} alt={title} />
+          </div>
+        </Link>
+        <div className="producto_footer">
+          <h1>{title}</h1>
+          <p>{category}</p>
+          <p className="price">${price}</p>
+        </div>
+        <div class="buttom">
+          <button className="btn" onClick={() => addCarrito(id)}>Añadir al carrito</button>
+          <div>
+            <Link to={`/producto/${id}`} className="btn">
+              Vista
+            </Link>
+          </div>
+        </div>
+      </div>
+    )
+}
